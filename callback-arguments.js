8268ef816445c4e68a7223892ed2c0627506fbba
sam dunner <samdunner23@gmail.com>
2016-06-23 18:41:53 +0000

    additional changes made with forEach

--- callback-arguments.js
+++ callback-arguments.js
@@ -10,3 +10,17 @@ function actionWhenFound(index) {
   console.log("His index is " + index + " , found him!");
 }
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function (actionWhenFound, index) {
+    if (arr[index] == "Waldo") {
+      found(index);  // execute callback
+    }
+  });
+}
+function actionWhenFound(index) {
+  console.log("His index is " + index + " , found him!");
+}
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
